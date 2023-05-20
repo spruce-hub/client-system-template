7dafca9967b2037f426f0322b7031f6ddbd54140
liaozheng-yi <813144314@qq.com>
2023-05-20 12:03:32 +0800

    chore: 依赖迭代

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,17 +62,6 @@ export default defineNuxtConfig({
     excludePath: ['/login', '/register'],
   },
   postcss: {
-    plugins: {
-      'postcss-pxtorem': {
-        rootValue: 10, // 指定转换倍率，现在设置这个表示1rem=10px;
-        propList: ['*'], // 属性列表，表示你要把哪些css属性的px转换成rem，这个*表示所有
-        mediaQuery: false, // 是否允许使用媒体查询，false媒体查询的代码可用，true不可用
-        exclude: /node_modules/,
-        replace: true, // 替换包含rem的规则，而不是添加回退
-        minPixelValue: 2, // 需要转换的最小值
-        unitPrecision: 5, // 转换成rem单位的小数点后的保留位数
-        selectorBlackList: ['html', 'f-1', 'f-2', 'el-'], // 匹配不被转换为rem的选择器
-      },
-    },
+    plugins: {},
   },
 })
